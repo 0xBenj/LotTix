312d25081de72845bd40f1e8261131e218254b1c
gwarren3210 <gavrielewarren@gmail.com>
2025-04-16 00:21:13 -0400

    Fix: Navbar improper scaling

---
+++ lottix-app/src/components/HeroBanner.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import styled from 'styled-components';
+
+interface HeroBannerProps {}
+
+const HeroBanner: React.FC<HeroBannerProps> = () => {
+  return (
+    <HeroBannerContainer>
+      <MainHeading>Win Concert Tickets</MainHeading>
+      <SubHeading>For Just $3</SubHeading>
+      <Description>Enter our lottery for a chance to see your favorite artists live!</Description>
+      <SearchButton>Search Events</SearchButton>
+    </HeroBannerContainer>
+  );
+};
+
+export default HeroBanner;
+
+const HeroBannerContainer = styled.div`
+  background-color: #1e2b7d;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 3rem;
+  text-align: center;
+  color: white;
+`;
+
+const MainHeading = styled.h2`
+  font-size: 2.5rem;
+  font-weight: bold;
+  margin-bottom: 0.5rem;
+`;
+
+const SubHeading = styled.h3`
+  font-size: 2rem;
+  font-weight: bold;
+  margin-bottom: 1.5rem;
+`;
+
+const Description = styled.p`
+  margin-bottom: 2rem;
+  font-size: 1rem;
+`;
+
+const SearchButton = styled.button`
+  background-color: #DA1692;
+  color: white;
+  padding: 0.75rem 2rem;
+  border-radius: 0.375rem;
+  font-weight: 500;
+  border: none;
+  cursor: pointer;
+  transition: background-color 0.2s;
+
+  &:hover {
+    background-color: #C31884;
+  }
+`;
